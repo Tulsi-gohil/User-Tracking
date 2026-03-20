c16c220c25702c702912ef30efebc6e6f74f9613
Tulsi-gohil <gohiltulsi451@gmail.com>
2026-03-20 15:42:29 +0530

    update

--- frontend/src/otpverify.js
+++ frontend/src/otpverify.js
@@ -29,7 +29,7 @@ function OtpVerify() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/verifyOtp",
+        "https://user-tracking-1.onrender.com/api/auth/verifyOtp",
         { email, otp }
       );
 
