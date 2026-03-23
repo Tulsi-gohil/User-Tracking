24bb4b43546e0fde2533f82705933ba38d268e77
Tulsi-gohil <gohiltulsi451@gmail.com>
2026-03-23 21:37:46 +0530

    update

--- frontend/src/admin.js
+++ frontend/src/admin.js
@@ -96,7 +96,7 @@ export default function AdminPanel() {
               alignItems: "center",
             }}
           >
-            <button onClick={() => setSelectedShortId(null)} className="btn-close">✕</button>
+            <button onClick={() => setSelectedShortId(null)}>✕</button>
           </div>
       <div className="log row py-5">
           {visitorLogs.length > 0 ? (
