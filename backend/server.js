35a6af55e0228ddf80e0dd056e770d780d49c80b
Tulsi-gohil <gohiltulsi451@gmail.com>
2026-03-24 12:46:41 +0530

    vercel add

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,8 @@ app.use('/proxy', createProxyMiddleware({
   changeOrigin: true,
   selfHandleResponse: false
 }));
-  
-app.listen(5000, () => console.log("Server running on port 5000"));
+
+ const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
